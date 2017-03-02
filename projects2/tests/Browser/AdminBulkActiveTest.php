f641928a9b4bcf01e4ab458eb936d020b45527be
Billy <william.allan@glasgow.ac.uk>
2017-03-02 09:10:03 +0000

    Test/fixups for bulk active/inactive page

---
+++ projects2/tests/Browser/AdminBulkActiveTest.php
@@ -0,0 +1,31 @@
+<?php
+// @codingStandardsIgnoreFile
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AdminBulkActiveTest extends DuskTestCase
+{
+    /** @test */
+    public function admin_can_bulk_set_project_is_active_flags()
+    {
+        $admin = $this->createAdmin();
+        $project1 = $this->createProject(['is_active' => false]);
+        $project2 = $this->createProject(['is_active' => true]);
+
+        $this->browse(function ($browser) use ($project1, $project2, $admin) {
+            $browser->loginAs($admin)
+                    ->visit('/')
+                    ->clickLink('Bulk Active/Inactive')
+                    ->assertSee('Projects - Bulk Inactive/Active')
+                    ->assertSee($project1->title)
+                    ->assertSee($project2->title)
+                    ->radio("statuses[{$project1->id}]", 1)
+                    ->radio("statuses[{$project2->id}]", 0)
+                    ->press('Update')
+                    ->assertSee('Changes saved');
+        });
+    }
+}
