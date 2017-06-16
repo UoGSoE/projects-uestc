85072b2ee1c34dbcbde2b7a92788ab969db2bd8c
Billy <william.allan@glasgow.ac.uk>
2017-06-16 10:39:22 +0100

    More JS fixups...

--- projects2/resources/assets/js/student_project_chooser.js
+++ projects2/resources/assets/js/student_project_chooser.js
@@ -11,9 +11,9 @@ Vue.component('project-detail', {
     <div class="panel panel-default">
         <div class="panel-heading fake-link" :id="'title_' + project.id" @click="toggleDetails">
             <h3 class="panel-title">
-                @{{ project.title }} (@{{ project.owner }})
+                {{ project.title }} ({{ project.owner }})
                 <span v-if="project.discipline">
-                    (field @{{ project.discipline }})
+                    (field {{ project.discipline }})
                 </span>
                 <span style="float:right">
                     <img :src="'img/'+project.institution+'.png'" :alt="project.institution" height="20" width="30">
@@ -23,20 +23,20 @@ Vue.component('project-detail', {
         <transition name="fade">
         <div v-if="showDetails">
             <div class="panel-body" >
-                @{{ project.description }}
+                {{ project.description }}
                 <div class="help-block">
-                    Prerequisites: @{{ project.prereq }}
+                    Prerequisites: {{ project.prereq }}
                 </div>
             </div>
             <ul class="list-group">
                 <li class="list-group-item" v-for="link in project.links">
                     <a :href="link.url" target="_blank">
-                        @{{ link.url}}
+                        {{ link.url}}
                     </a>
                 </li>
                 <li class="list-group-item" v-for="file in project.files">
                     <a :href="'/projectfile/' + file.id">
-                        <span class="glyphicon glyphicon-download" aria-hidden="true"></span> @{{ file.original_filename }}
+                        <span class="glyphicon glyphicon-download" aria-hidden="true"></span> {{ file.original_filename }}
                     </a>
                 </li>
             </ul>
@@ -45,7 +45,7 @@ Vue.component('project-detail', {
                     <div class="progress" style="float:left; width:50%; background-color:white">
                         <div :class="'progress-bar '+ project.popularity.colour" role="progressbar" :aria-valuenow="project.popularity.percent"
                           aria-valuemin="0" aria-valuemax="100" :style="'min-width: 2em; max-width:100%; width:'+project.popularity.percent+'%'">
-                            @{{ project.popularity.caption }}
+                            {{ project.popularity.caption }}
                         </div>
                     </div>
                     <div class="checkbox" style="float:right; margin-top:0px;">
