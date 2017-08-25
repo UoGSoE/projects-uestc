76a08957289fcc6a89fb7c846e6c392616d1782c
Finlay Maciver <finlay.maciver@glasgow.ac.uk>
2017-08-25 12:12:14 +0100

    Changed wording on import page

--- resources/views/user/import.blade.php
+++ resources/views/user/import.blade.php
@@ -2,13 +2,16 @@
 
 @section ('content')
     <h2>
-        Import a list of staff.
+        Import a list of EXTERNAL staff.
     </h2>
     <form method="POST" action="{!! route('staff.do_import') !!}" enctype="multipart/form-data">
         {{ csrf_field() }}
         <div class="form-group">
             <label for="inputFile">Upload an Excel file</label>
             <input type="file" id="inputFile" name="file" value="" required>
+            <p class="help-block">
+                This is ONLY for staff who do not have Glasgow GUIDs.
+            </p>
             <p class="help-block">
                 Spreadsheet must in of the form <pre>email|surname|forenames|institution</pre>
             </p>
