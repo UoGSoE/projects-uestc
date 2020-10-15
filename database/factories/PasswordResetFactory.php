07e3ba53744991f90f776a734093f42616273b5e
Laravel Shift <shift@laravelshift.com>
2020-10-15 10:14:51 +0000

    Adopt Laravel coding style
    The Laravel framework adopts the PSR-2 coding style with some additions.
    Laravel apps *should* adopt this coding style as well.
    
    However, Shift allows you to customize the adopted coding style by
    adding your own [PHP CS Fixer][1] `.php_cs` config to your project.
    
    You may use [Shift's .php_cs][2] file as a base.
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200

--- database/factories/PasswordResetFactory.php
+++ database/factories/PasswordResetFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\PasswordReset;
-use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class PasswordResetFactory extends Factory
 {
